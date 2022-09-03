5c1cc4da3e1d0b86c555722efe20ea711e281d3c
mkeloo <mkeloo@ufl.edu>
2022-09-03 01:18:46 -0400

    fetch api

---
+++ 18-fetch/app.js
@@ -0,0 +1,9 @@
+/*
+ Javascript Nuggets
+
+ Fetch API -  Browser API for HTTP (AJAX) Requests
+ Default - GET Requests, supports other methods as well
+ Returns Promise
+*/
+
+const url = 'https://www.course-api.com/react-tours-project'
